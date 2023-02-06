48619479020b7d43f4f6cdc04d1326f933e5acc7
Artem Zhukovskyi <mr.zhukovsky02@gmail.com>
2023-02-06 08:04:57 +0100

    #71: Added user profile tab at the navigation bar.

---
+++ src/components/SideBar/Profile/userProfile.tsx
@@ -0,0 +1,11 @@
+import { Col, Container, Row } from "react-bootstrap";
+import "./styles.css";
+
+const UserProfile = () => { 
+  return(
+    <Container className="user-profile">
+    </Container>
+  );
+}
+
+export default UserProfile;
